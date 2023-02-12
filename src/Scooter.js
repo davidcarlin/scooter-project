8998149d762d8d0bed0ceaac7f22e0f4195f5d9d
davidcarlin <david.carlin@barclays.com>
2023-02-12 21:11:23 +0000

    tests and classes done for user and scooter, all 16 tests passed

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,46 @@
-class Scooter{
-  // scooter code here
+class Scooter {
+  static nextSerial = 1;
+
+  constructor(station) {
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial++;
+    this.charge = 100;
+    this.isBroken = false;
+  }
+
+  rent() {
+    if (this.charge <= 20) {
+      throw new Error("Scooter needs to charge");
+    }
+    if (this.isBroken) {
+      throw new Error("Scooter needs repair");
+    }
+    this.user = "exampleUser";
+    this.station = null;
+  }
+
+  dock(station) {
+    this.station = station;
+    this.user = null;
+  }
+
+  async recharge() {
+    console.log("Starting charge");
+
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // wait 2 seconds
+    this.charge = 100;
+
+    console.log("Charge complete");
+  }
+
+  requestRepair() {
+    let repairInterval = setInterval(() => {
+      this.isBroken = false;
+      console.log("Repair completed");
+      clearInterval(repairInterval);
+    }, 5000);
+  }
 }
 
-
-module.exports = Scooter
+module.exports = Scooter;
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,51 +1,86 @@
 const Scooter = require("../src/Scooter");
 const User = require("../src/User");
-// Scooter class test suite
+
+// Scooter class tests
 describe("Scooter class tests", () => {
-  let scooter;
-  // test scooterId
-  test("scooterId property is valid", () => {
-    scooter = new Scooter("scooter1");
-    expect(scooter.scooterId).toBe("scooter1");
-  });
   // test station
-  test("station property is valid", () => {
-    scooter = new Scooter("scooter1");
-    expect(scooter.station).toBe(null);
-  });
-  //isRented property
-  test("isRented property is valid", () => {
-    scooter = new Scooter("scooter1");
-    expect(scooter.isRented).toBe(false);
-  });
-  //rent method
-  test("rent method converts isRented property to true", () => {
-    scooter = new Scooter("scooter1");
-    scooter.rent();
-    expect(scooter.isRented).toBe(true);
-  });
-  //return method
-  test("return method converts isRented property to false", () => {
-    scooter = new Scooter("scooter1");
-    scooter.rent();
-    scooter.return();
-    expect(scooter.isRented).toBe(true);
-  });
-  //dock method
-  test("dock method gives valid station property", () => {
-    scooter = new Scooter("scooter1");
-    scooter.dock("station1");
-    expect(scooter.station).toBe("station1");
-  });
-  //undock method
-  test("undock method sets station property to null", () => {
-    scooter = new Scooter("scooter1");
-    scooter.dock("station1");
-    scooter.undock();
-    expect(scooter.station).toBe(null);
+  test("scooter has a station", () => {
+    let station = "Belfast";
+    let scooter = new Scooter(station);
+    expect(scooter.station).toBe(station);
   });
 
-  //requestRepair method
+  // test null user default
+  test("scooter doesn't have user by default", () => {
+    let scooter = new Scooter("Belfast");
+    expect(scooter.user).toBeNull();
+  });
 
-  //charge method
+  // test full charge default
+  test("scooter has full charge by default", () => {
+    let scooter = new Scooter("Belfast");
+    expect(scooter.charge).toBe(100);
+  });
+
+  // test not broken by default
+  test("scooter is not broken by default", () => {
+    let scooter = new Scooter("Belfast");
+    expect(scooter.isBroken).toBe(false);
+  });
+
+  // test rent method
+  test("scooter can be rented", () => {
+    let station = "Belfast";
+    let scooter = new Scooter(station);
+    scooter.rent();
+    expect(scooter.station).toBeNull();
+    expect(scooter.user).toBe("exampleUser");
+  });
+
+  // test error when trying to rent scooter with low charge
+  test("scooter can't be rented when charge is not more than 20", () => {
+    let station = "Belfast";
+    let scooter = new Scooter(station);
+    scooter.charge = 19;
+    expect(() => scooter.rent()).toThrowError("Scooter needs to charge");
+  });
+
+  // test error when trying to rent a broken scooter
+  test("can't rent a broken scooter - throws an error", () => {
+    let station = "Belfast";
+    let scooter = new Scooter(station);
+    scooter.isBroken = true;
+    expect(() => scooter.rent()).toThrowError("Scooter needs repair");
+  });
+
+  // test dock method
+  test("scooter can be docked", () => {
+    let station = "Belfast";
+    let scooter = new Scooter(station);
+    let newStation = "Derry";
+    scooter.rent();
+    scooter.dock(newStation);
+    expect(scooter.station).toBe(newStation);
+    expect(scooter.user).toBeNull();
+  });
+
+  // test charge method
+  test("scooter can be charged", async () => {
+    let station = "Belfast";
+    let scooter = new Scooter(station);
+    scooter.charge = 19;
+    await scooter.recharge(); // we need to wait for the charge!
+    expect(scooter.charge).toBe(100);
+  });
+
+  // test repair method
+  test("scooter can be repaired", () => {
+    let station = "Belfast";
+    let scooter = new Scooter(station);
+    scooter.isBroken = true;
+    scooter.requestRepair();
+    setTimeout(() => {
+      expect(scooter.isBroken).toBe(false);
+    }, 5000);
+  });
 });
